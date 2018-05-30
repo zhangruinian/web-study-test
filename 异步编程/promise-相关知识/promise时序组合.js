776792aae069b746568040ae98853f3468589625
“frontIT” <zhangruinian3@gami.com>
2018-05-30 17:56:43 +0800

    增加多种css布局

--- 异步编程/promise-相关知识/promise时序组合.js
+++ 异步编程/promise-相关知识/promise时序组合.js
@@ -1,7 +1,8 @@
 // [func1, func2].reduce((p, f) => p.then(f), Promise.resolve())
 
-let applyAsync = (acc,val) => acc.then(val);
+let applyAsync = (acc,cur) => acc.then(cur);
 let composeAsync = (...funcs) => x => funcs.reduce(applyAsync, Promise.resolve(x));
+
 
 // es7
 
